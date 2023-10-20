bd3bb46d7477522e536a2512cece56f20336f069
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2023-10-20 18:47:11 -0300

    Criação do arquivo quiz.html e ajustes css

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const menuIcon = document.querySelector("#menuIcon");
 
 menuIcon.onclick = function() {
     let menu = document.querySelector("nav");
-    if (menu.style.transform == "translateY(9vh)") {
+    if (menu.style.transform == "translateY(85px)") {
         menuIcon.style.transform = "rotateZ(0deg)";
         menu.style.transform = "translateY(-23vh)";
     }
     else {
         menuIcon.style.transform = "rotateZ(90deg)";
-        menu.style.transform = "translateY(9vh)";
+        menu.style.transform = "translateY(85px)";
     }
 }
